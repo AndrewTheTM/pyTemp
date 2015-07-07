ced2cd96053533febf0801ce5275920fcde51933
Andrew Rohne <ke8p@ke8p.us>
2015-07-06 20:54:27 -0400

    finished (?) schedule lookup on pyTrigger

--- pyTrigger.py
+++ pyTrigger.py
@@ -1,6 +1,8 @@
 import smbus
 import RPi.GPIO as GPIO
 import sqlite3
+import datetime
+import time
 
 DATABASE_PATH = '/var/www/db/fermpi.db'
 I2C_ADDRESS = 0x4c
@@ -12,30 +14,49 @@ GPIO.setup(18,GPIO.OUT)
 # 1. get current set point
 conn = sqlite3.connect(DATABASE_PATH)
 cStatus = conn.cursor()
-cStatus.execute("SELECT * FROM fermStatus")
+cStatus.execute("SELECT eventID, timeStamp FROM fermStatus")
 status = cStatus.fetchall()
 
-cPlan = conn.cursor()
-cPlan.execute("SELECT * FROM fermSchedules") #WHERE...
-plan = cPlan.fetchall()
+if(len(status)>0):
+	eventID = status[0][0]
+	startTimeStamp = status[0][1] #timestamp in seconds
 
-#print plan
+	cPlan = conn.cursor()
+	cPlan.execute("SELECT fermScheduleId, profileName, primaryDays, primaryTemp, diacetylRestDays, diacetylRestTemp, lagerDays, lagerTemp FROM fermSchedules WHERE fermScheduleId = ?",(eventID,))
+	plan = cPlan.fetchall()
 
-sVal=50 #Debugging
+	cTime = time.time()
+	enlapsedTime = datetime.timedelta(seconds=cTime-startTimeStamp)
+	priTime = plan[0][2]
+	diaTime = plan[0][4]
+	lagTime = plan[0][6]
+
+	if(enlapsedTime.days<=priTime):
+		print "In Primary"
+		sVal = plan[0][3]
+	elif(enlapsedTime.days<=(priTime+diaTime)):
+		print "In diacetyl rest"
+		sVal = plan[0][5]
+	elif(enlapsedTime.days<=(priTime+diaTime+lagTime)):
+		print "In Lager"
+		sVal = plan[0][7]
+	else:
+		print "past lager"
+		sVal = plan[0][7]
+		#Holds to the lager temperature
+else:
+	sVal = 70
+	
+print sVal
 
 # 2. get current temperature
 bus = smbus.SMBus(0)
 value = bus.read_i2c_block_data(I2C_ADDRESS,0)
 intValue = (value[1] + (256 * value[0])) >> 2
-#print value #Debug
-print intValue #Debug
 volts = 3.3 * intValue / 1024.0
-#print volts #Debug
 rtwo = 100.0* ((3.3 / volts) - 1.0)
-#print rtwo #Debug
 
 tempC = (rtwo - 100.0) / 0.39
-#print tempC #Debug
 
 tempF = 9.0 / 5.0 * tempC + 32.0
 tempF = round(tempF * 10.0) / 10.0
@@ -49,4 +70,3 @@ if(tempF>sVal):
 else:
 	GPIO.output(18,GPIO.LOW)
 	print "Turn off"
-
